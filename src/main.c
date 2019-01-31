b070f103bc6950d42c6e8d87b0aaa1f729555ada
lupyuen <luppy@appkaki.com>
2019-01-31 11:43:16 +0800

    Build OK

--- src/main.c
+++ src/main.c
@@ -1,23 +1,22 @@
 #include <logger.h>
-
-double x = 123;
-double y = 456;
-double r;
+#ifdef UNIT_TEST
+extern int test_nanofloat(void);
+#endif  //  UNIT_TEST
 
 int main(void)
 {	
 	enable_log();          //  Enable logging via Arm Semihosting. Note: ST Link must be connected or this line will hang.
 	debug_println("Starting..."); debug_force_flush();
 
-	r = x * y;             //  r = 123 * 456
-	debug_print_float(r);  //  Print the value of 123 * 456.
+#ifdef UNIT_TEST
+	test_nanofloat();
+#endif  //  UNIT_TEST
 
-	debug_println("");
+	debug_println("Done");
 	debug_force_flush();   //  Flush the debug buffer before we halt.
 	for (;;) {}            //  Loop forever.
 }
 
-//  double-precision multiplication
-double __wrap___aeabi_dmul(double x, double y) { 
-    return 123456; 
-}
+#ifdef UNIT_TEST
+#include "../lib/nano-float/test/test.c"
+#endif  //  UNIT_TEST
